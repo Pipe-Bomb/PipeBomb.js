4bca49cf37add0a705b3ab7abe6e57e14bea9664
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2023-02-19 12:36:48 +1300

    remove debug code

--- src/index.ts
+++ src/index.ts
@@ -19,33 +19,3 @@ export default class MusicService {
         this.v1 = new V1(this.context, this.trackCache, this.collectionCache);
     }
 }
-
-const service = new MusicService("http://localhost:8000", "dkcxLLnhjgHKphreADgFmX7r31r0RHTtY6C9oaf3");
-
-
-
-(async () => {
-    // const playlist = (await service.v1.getPlaylists())[0];
-    // await (await playlist.getTrackList(service.trackCache))[0].getMetadata();
-    // console.log(playlist);
-
-    // const tracks = await service.v1.search("SoundCloud", "magenta dnb");
-    // const track = tracks[0];
-    // await playlist.addTracks(track);
-
-    // console.log(await service.v1.createPlaylist("Cool New Playlist", tracks));
-    try {
-        const playlist = await service.v1.getPlaylist(11);
-        await playlist.deleteCollection();
-    } catch (e) {
-        console.log(e);
-    }
-    
-    // console.log(playlist);
-    // try {
-    //     
-    // } catch (e) {
-    //     // console.log(e);
-    // }
-    console.log("done!");
-})();
